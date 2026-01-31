b3790fc3f0f30ad3c4729ae4d9dcb9256c9e8f3d
abdulahad-009 <abdul.ahad@codinova.com>
2026-01-31 19:35:54 +0530

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("JS is running");
-
+document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const navMenu = document.getElementById("navMenu");
-
-    if (!hamburger || !navMenu) {
-        console.error("Elements not found");
-        return;
-    }
 
     hamburger.addEventListener("click", () => {
         navMenu.classList.toggle("active");
